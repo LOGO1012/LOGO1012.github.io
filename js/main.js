bc9318fded7e28cd35cd73fa49df0515acb354ce
LDS <ekthf988172@gmail.com>
2024-05-05 19:42:39 +0900

    main

--- js/main.js
+++ js/main.js
@@ -67,12 +67,19 @@ window.addEventListener('scroll', _.throttle(
 // 현재 활성화된 슬라이드의 인덱스를 추적하는 변수
 let currentSlide = 0;
 
+// 페이지 로드 시 초기화 함수
+document.addEventListener('DOMContentLoaded', function() {
+  updateSlideCount(); // 슬라이드 개수 업데이트
+  updateCurrentSlide(); // 현재 슬라이드 번호 업데이트
+});
+
 // 이전 슬라이드로 이동하는 함수
 function prevSlide() {
   const slides = document.querySelectorAll('.slide');
   slides[currentSlide].classList.remove('active');
   currentSlide = (currentSlide - 1 + slides.length) % slides.length;
   slides[currentSlide].classList.add('active');
+  updateCurrentSlide();
 }
 
 // 다음 슬라이드로 이동하는 함수
@@ -81,5 +88,19 @@ function nextSlide() {
   slides[currentSlide].classList.remove('active');
   currentSlide = (currentSlide + 1) % slides.length;
   slides[currentSlide].classList.add('active');
+  updateCurrentSlide();
 }
+
+// 슬라이드 개수 업데이트 함수
+function updateSlideCount() {
+  const totalSlides = document.querySelectorAll('.slide').length;
+  document.getElementById('slideCount').textContent = `/ ${totalSlides}`;
+}
+
+// 현재 슬라이드 번호 업데이트 함수
+function updateCurrentSlide() {
+  document.getElementById('currentSlide').textContent = currentSlide + 1;
+}
+
+
 
